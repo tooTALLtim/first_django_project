4af52eb1d123e381e20d3579101a243928dce24f
Tim Davis <mitted2@gmail.com>
2023-02-21 13:03:54 -0700

    App configured for deploy

--- storefront/settings/common.py
+++ storefront/settings/common.py
@@ -163,33 +163,22 @@ SIMPLE_JWT = {
 }
 
 #these are all the defaults, but written out for learning
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'localhost'
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-EMAIL_PORT = 2525
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'localhost'
+# EMAIL_HOST_USER = ''
+# EMAIL_HOST_PASSWORD = ''
+# EMAIL_PORT = 2525
 DEFAULT_FROM_EMAIL = 'timmay@yo.com'
 
 ADMINS = [
     ('Tim', 'admin@domain.com')
 ]
 
-CELERY_BROKER_URL = 'redis://localhost:6379/1'
 CELERY_BEAT_SCHEDULE = {
     'notify_customers': {
         'task': 'playground.tasks.notify_customers',
         'schedule': 5,
         'args': ['Hello my beautiful!']
-    }
-}
-
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/2",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
     }
 }
 
--- storefront/settings/dev.py
+++ storefront/settings/dev.py
@@ -14,3 +14,20 @@ DATABASES = {
         'PASSWORD': 'datab4s3!'
     }
 }
+
+CELERY_BROKER_URL = 'redis://localhost:6379/1'
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/2",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+EMAIL_HOST = 'localhost'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 2525
